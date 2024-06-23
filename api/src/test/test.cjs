e2b39fcbb8f3bcee2afd842504857f770361404f
Henry Eduardo Giraldo Betancur <henrygiraldo27@gmail.com>
2024-06-23 18:41:12 -0500

    Update test.cjs

--- api/src/test/test.cjs
+++ api/src/test/test.cjs
@@ -1,13 +1,16 @@
 const axios = require('axios');
 const assert = require('assert');
 
+// Obtén la URL base de la API desde una variable de entorno o constante
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000';
+
 describe('Pruebas de API', function() {
     this.timeout(5000); // Extiende el tiempo de espera en caso de que las solicitudes tarden más de lo esperado
 
     describe('Endpoint /api/totalventas', function() {
         it('debería devolver resultados', async function() {
             try {
-                const response = await axios.get('http://localhost:3000/api/totalventas');
+                const response = await axios.get(`${API_BASE_URL}/api/totalventas`);
                 assert(response.data.length > 0, 'La respuesta no contiene datos.');
                 console.log('Pruebas correctas: La respuesta contiene datos.');
             } catch (error) {
@@ -19,7 +22,7 @@ describe('Pruebas de API', function() {
     describe('Endpoint /api/region', function() {
         it('debería devolver resultados', async function() {
             try {
-                const response = await axios.get('http://localhost:3000/api/region');
+                const response = await axios.get(`${API_BASE_URL}/api/region`);
                 assert(response.data.length > 0, 'La respuesta no contiene datos.');
                 console.log('Pruebas correctas: La respuesta contiene datos.');
             } catch (error) {
@@ -28,39 +31,5 @@ describe('Pruebas de API', function() {
         });
     });
 
-    describe('Endpoint /api/productos', function() {
-        it('debería devolver resultados', async function() {
-            try {
-                const response = await axios.get('http://localhost:3000/api/productos');
-                assert(response.data.length > 0, 'La respuesta no contiene datos.');
-                console.log('Pruebas correctas: La respuesta contiene datos.');
-            } catch (error) {
-                assert.fail(`Error en la solicitud: ${error.message}`);
-            }
-        });
-    });
-
-    describe('Endpoint /api/vendedores', function() {
-        it('debería devolver resultados', async function() {
-            try {
-                const response = await axios.get('http://localhost:3000/api/vendedores');
-                assert(response.data.length > 0, 'La respuesta no contiene datos.');
-                console.log('Pruebas correctas: La respuesta contiene datos.');
-            } catch (error) {
-                assert.fail(`Error en la solicitud: ${error.message}`);
-            }
-        });
-    });
-
-    describe('Endpoint /api/tableData', function() {
-        it('debería devolver resultados', async function() {
-            try {
-                const response = await axios.get('http://localhost:3000/api/tableData');
-                assert(response.data.length > 0, 'La respuesta no contiene datos.');
-                console.log('Pruebas correctas: La respuesta contiene datos.');
-            } catch (error) {
-                assert.fail(`Error en la solicitud: ${error.message}`);
-            }
-        });
-    });
+    // Resto de pruebas similares...
 });
